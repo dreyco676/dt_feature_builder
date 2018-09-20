0fb20c23dc1e885f8fbad67c8d94c6aceb590d6b
John Hogue <dreyco676@gmail.com>
2018-09-20 14:13:22 -0500

    reordered file

--- dt_feature_builder.py
+++ dt_feature_builder.py
@@ -3,57 +3,6 @@ from datetime import date
 import calendar
 
 
-def week_of_month(target_date):
-    # https://stackoverflow.com/questions/25249033/week-of-a-month-pandas
-    target_date = target_date.to_datetime()
-
-    days_this_month = calendar.mdays[target_date.month]
-    for i in range(1, days_this_month):
-        d = datetime(target_date.year, target_date.month, i)
-        if d.day - d.weekday() > 0:
-            start_date = d
-            break
-    # now we can use the modulo 7 appraoch
-    week_of_month = (target_date - start_date).days // 7 + 1
-    return week_of_month
-
-
-def day_of_quarter(target_date)
-    # calculate the day in the quarter
-    target_date = target_date.to_datetime()
-    
-    current_quarter = round((current_date.month - 1) / 3 + 1)
-    current_year = target_date.year
-    # current year quarter start dates
-    q1 = date(current_year, 1, 1)
-    q2 = date(current_year, 4, 1)
-    q3 = date(current_year, 7, 1)
-    q4 = date(current_year, 10, 1)
-    # calucate number of days since quarter start
-    if current_quarter == 1:
-        day_of_quarter = target_date - q1
-    elif current_quarter == 2:
-        day_of_quarter = target_date - q2
-    elif current_quarter == 3:
-        day_of_quarter = target_date - q3
-    elif current_quarter == 4:
-        day_of_quarter = target_date - q4
-    return day_of_quarter
-
-
-def week_of_quarter(target_date)
-    # calculate the day in the quarter
-    return week_of_quarter
-
-
-def month_of_quarter(target_date)
-    # calculate the month in the quarter
-    target_date = target_date.to_datetime()
-    target_month = target_date.month - 1
-    month_of_quarter = (target_month % 3) + 1
-    return month_of_quarter
-
-
 def datetime_feature_generator(df, date_col, freq):
     """
     Partition a datetime column into new feature columns
@@ -122,3 +71,53 @@ def yearly_feature_generator(df, date_col):
     # year as int
     df['year'] = df[date_col].dt.year
     return df
+
+def week_of_month(target_date):
+    # https://stackoverflow.com/questions/25249033/week-of-a-month-pandas
+    target_date = target_date.to_datetime()
+
+    days_this_month = calendar.mdays[target_date.month]
+    for i in range(1, days_this_month):
+        d = datetime(target_date.year, target_date.month, i)
+        if d.day - d.weekday() > 0:
+            start_date = d
+            break
+    # now we can use the modulo 7 appraoch
+    week_of_month = (target_date - start_date).days // 7 + 1
+    return week_of_month
+
+
+def day_of_quarter(target_date)
+    # calculate the day in the quarter
+    target_date = target_date.to_datetime()
+    
+    current_quarter = round((current_date.month - 1) / 3 + 1)
+    current_year = target_date.year
+    # current year quarter start dates
+    q1 = date(current_year, 1, 1)
+    q2 = date(current_year, 4, 1)
+    q3 = date(current_year, 7, 1)
+    q4 = date(current_year, 10, 1)
+    # calucate number of days since quarter start
+    if current_quarter == 1:
+        day_of_quarter = target_date - q1
+    elif current_quarter == 2:
+        day_of_quarter = target_date - q2
+    elif current_quarter == 3:
+        day_of_quarter = target_date - q3
+    elif current_quarter == 4:
+        day_of_quarter = target_date - q4
+    return day_of_quarter
+
+
+def week_of_quarter(target_date)
+    # calculate the day in the quarter
+    return week_of_quarter
+
+
+def month_of_quarter(target_date)
+    # calculate the month in the quarter
+    target_date = target_date.to_datetime()
+    target_month = target_date.month - 1
+    month_of_quarter = (target_month % 3) + 1
+    return month_of_quarter
